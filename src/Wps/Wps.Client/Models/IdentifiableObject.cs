1c06ab12bc987bd97fcef6d3cacce0db8cbd689a
Mihai Stan <stanmihai17cs@gmail.com>
2019-05-27 13:49:38 +0200

    Add base model for identifiable objects

---
+++ src/Wps/Wps.Client/Models/IdentifiableObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Serialization;
+using Wps.Client.Utils;
+
+namespace Wps.Client.Models
+{
+    /// <summary>
+    /// Represents an object that has properties allowing itself to be identified.
+    /// </summary>
+    public abstract class IdentifiableObject
+    {
+
+        /// <summary>
+        /// Human readable title for the object. (i.e. Input, Output, Process, etc.)
+        /// </summary>
+        /// <remarks>
+        /// This property is required and shall not be null!
+        /// </remarks>
+        [XmlElement("Title", Namespace = ModelNamespaces.Ows)]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Human readable short description of the object. (i.e. Input, Output, Process, etc.)
+        /// </summary>
+        [XmlElement("Abstract", Namespace = ModelNamespaces.Ows)]
+        public string Abstract { get; set; }
+
+        /// <summary>
+        /// Unambiguous identifier of the object. (i.e. Input, Output, Process, etc.)
+        /// </summary>
+        [XmlElement("Identifier", Namespace = ModelNamespaces.Ows)]
+        public string Identifier { get; set; }
+
+    }
+}
