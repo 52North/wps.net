22360b7e3849948e24fa3efdc96f1c4fae176e8d
Mihai Stan <stanmihai17cs@gmail.com>
2019-05-31 15:14:31 +0200

    Add GetCapabilities request with serialization test

---
+++ src/Wps/Wps.Client.Tests/ModelSerializationTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using System.Text.RegularExpressions;
+using Wps.Client.Models.Requests;
+using Wps.Client.Services;
+using Xunit;
+
+namespace Wps.Client.Tests
+{
+    public class ModelSerializationTests : IClassFixture<XmlSerializationService>
+    {
+
+        private readonly XmlSerializationService _serializer;
+
+        public ModelSerializationTests(XmlSerializationService serializer)
+        {
+            _serializer = serializer;
+        }
+
+        [Fact]
+        public void SerializeGetCapabilitiesRequest_ValidRequestGiven_ShouldPass()
+        {
+            const string expectedXml = @"<?xml version=""1.0"" encoding=""utf-8""?><GetCapabilities xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" service=""WPS"" xmlns=""http://www.opengis.net/wps/2.0"" />";
+
+            // Remove white spaces and new line characters. They do not change the actual (de)serialization of the XML.
+            var trimmedExpectedXml = Regex.Replace(expectedXml, @"\s+", string.Empty);
+
+            var request = new GetCapabilitiesRequest()
+            {
+                Service = "WPS"
+            };
+
+            var resultXml = _serializer.Serialize(request);
+            var trimmedResult = Regex.Replace(resultXml, @"\s+", string.Empty);
+            trimmedResult.Should().Be(trimmedExpectedXml);
+        }
+
+    }
+}
---
+++ src/Wps/Wps.Client/Models/Requests/GetCapabilitiesRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml.Serialization;
+using Wps.Client.Utils;
+
+namespace Wps.Client.Models.Requests
+{
+    [Serializable]
+    [XmlRoot("GetCapabilities", Namespace = ModelNamespaces.Wps)]
+    public class GetCapabilitiesRequest
+    {
+        /// <summary>
+        /// The concerned service by the GetCapabilities request.
+        /// </summary>
+        [XmlAttribute("service", Namespace = ModelNamespaces.Wps)]
+        public string Service { get; set; } = "WPS";
+    }
+}
