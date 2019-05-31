8dc2947e688bd7397911557ed1df836452749908
Mihai Stan <stanmihai17cs@gmail.com>
2019-05-31 16:13:32 +0200

    Add DescribeProcess request with serialization test

--- src/Wps/Wps.Client.Tests/ModelSerializationTests.cs
+++ src/Wps/Wps.Client.Tests/ModelSerializationTests.cs
@@ -34,5 +34,29 @@ namespace Wps.Client.Tests
             trimmedResult.Should().Be(trimmedExpectedXml);
         }
 
+        [Fact]
+        public void SerializeDescribeProcessRequest_ValidRequestGiven_ShouldPass()
+        {
+            const string expectedXml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+                                         <wps:DescribeProcess xmlns:ows=""http://www.opengis.net/ows/2.0"" xmlns:xli=""http://www.w3.org/1999/xlink"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" service=""WPS"" version=""2.0.0"" lang=""en-US"" xmlns:wps=""http://www.opengis.net/wps/2.0"">
+                                           <ows:Identifier>id1</ows:Identifier>
+                                           <ows:Identifier>id2</ows:Identifier>
+                                           <ows:Identifier>id3</ows:Identifier>
+                                         </wps:DescribeProcess>";
+
+            // Remove white spaces and new line characters. They do not change the actual (de)serialization of the XML.
+            var trimmedExpectedXml = Regex.Replace(expectedXml, @"\s+", string.Empty);
+
+            var request = new DescribeProcessRequest()
+            {
+                Identifiers = new[] {"id1", "id2", "id3"},
+                Language = "en-US",
+            };
+
+            var resultXml = _serializer.Serialize(request);
+            var trimmedResult = Regex.Replace(resultXml, @"\s+", string.Empty);
+            trimmedResult.Should().Be(trimmedExpectedXml);
+        }
+
     }
 }
---
+++ src/Wps/Wps.Client/Models/Requests/RequestBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+
+namespace Wps.Client.Models.Requests
+{
+    public abstract class RequestBase
+    {
+
+        /// <summary>
+        /// The type of the required service.
+        /// </summary>
+        [XmlAttribute("service")]
+        public string Service { get; set; } = "WPS";
+
+        /// <summary>
+        /// The version of the required service.
+        /// </summary>
+        [XmlAttribute("version")]
+        public string Version { get; set; } = "2.0.0";
+
+    }
+}
