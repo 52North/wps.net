5a331dbc2010cc4134bc4a8de182b08d093ae47d
Mihai Stan <stanmihai17cs@gmail.com>
2019-06-06 13:13:43 +0200

    Add LiteralDataValue for execution request

---
+++ src/Wps/Wps.Client/Models/Execution/LiteralDataValue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using Wps.Client.Utils;
+
+namespace Wps.Client.Models.Execution
+{
+    [XmlRoot("LiteralValue", Namespace = ModelNamespaces.Wps)]
+    public class LiteralDataValue : IXmlSerializable
+    {
+
+        public object Value { get; set; }
+
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            writer.WriteRaw(Value.ToString());
+        }
+    }
+}
