b83de21a328c3bf3cf336a54df9b4d066ec2ce26
Mihai Stan <stanmihai17cs@gmail.com>
2019-06-12 17:01:41 +0200

    Add GetCapabilities and DescribeProcess request to the WPS client

---
+++ src/Wps/Wps.Client.Tests/WpsClientTests.cs
@@ -0,0 +1,139 @@
+﻿using FluentAssertions;
+using Moq;
+using Moq.Protected;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Wps.Client.Models;
+using Wps.Client.Models.Execution;
+using Wps.Client.Models.Ows;
+using Wps.Client.Models.Requests;
+using Wps.Client.Services;
+using Xunit;
+
+namespace Wps.Client.Tests
+{
+    public class WpsClientTests
+    {
+
+        private const string MockUri = "http://mock.uri";
+
+        /*
+         * Helpers
+         */
+
+        private static HttpMessageHandler GetMockedMessageHandlerForResponse(string response, HttpStatusCode code = HttpStatusCode.OK)
+        {
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = code,
+                    Content = new StringContent(response, Encoding.UTF8)
+                })
+                .Verifiable();
+
+            return handlerMock.Object;
+        }
+
+        /*
+         * Get Capabilities Tests
+         */
+
+        [Theory]
+        [EmbeddedResourceData("Wps.Client.Tests/Resources/GetCapabilities.xml")]
+        public async Task GetCapabilities_ValidUriGiven_ShouldPass(string httpClientResponseXml)
+        {
+            var wpsClient = new WpsClient(new HttpClient(GetMockedMessageHandlerForResponse(httpClientResponseXml)), new XmlSerializationService());
+
+            var capabilities = await wpsClient.GetCapabilities(MockUri);
+            capabilities.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetCapabilities_NullUriGiven_ShouldThrow_ArgumentNullException()
+        {
+            var wpsClient = new WpsClient(new HttpClient(), new XmlSerializationService());
+            await Assert.ThrowsAsync<ArgumentNullException>(() => wpsClient.GetCapabilities(null));
+        }
+
+        /*
+         * Describe Process Tests
+         */
+
+        [Theory]
+        [EmbeddedResourceData("Wps.Client.Tests/Resources/ExceptionReport.xml")]
+        public async Task DescribeProcess_InvalidStringIdentifiersGiven_ShouldThrowOwsException(string httpClientResponseXml)
+        {
+            var wpsClient = new WpsClient(new HttpClient(GetMockedMessageHandlerForResponse(httpClientResponseXml, HttpStatusCode.BadRequest)), new XmlSerializationService());
+
+            await Assert.ThrowsAsync<OwsException>(() => wpsClient.DescribeProcess(MockUri, "invalid 1", "invalid 2"));
+        }
+
+        [Theory]
+        [EmbeddedResourceData("Wps.Client.Tests/Resources/ExceptionReport.xml")]
+        public async Task DescribeProcess_InvalidSummariesGiven_ShouldThrowOwsException(string httpClientResponseXml)
+        {
+            var summaries = new[]
+            {
+                new ProcessSummary {Identifier = "invalid 1"},
+                new ProcessSummary {Identifier = "invalid 2"}
+            };
+
+            var wpsClient = new WpsClient(new HttpClient(GetMockedMessageHandlerForResponse(httpClientResponseXml, HttpStatusCode.BadRequest)), new XmlSerializationService());
+
+            await Assert.ThrowsAsync<OwsException>(() => wpsClient.DescribeProcess(MockUri, summaries));
+        }
+
+        [Theory]
+        [EmbeddedResourceData("Wps.Client.Tests/Resources/ProcessOfferings.xml")]
+        public async Task DescribeProcess_ValidStringIdentifiersGiven_ShouldReturnTwoProcesses(string httpClientResponseXml)
+        {
+            var wpsClient = new WpsClient(new HttpClient(GetMockedMessageHandlerForResponse(httpClientResponseXml)), new XmlSerializationService());
+
+            var collection = await wpsClient.DescribeProcess(MockUri, "i.gensigset", "i.gensig");
+            collection.Should().HaveCount(2);
+        }
+
+        [Theory]
+        [EmbeddedResourceData("Wps.Client.Tests/Resources/ProcessOfferings.xml")]
+        public async Task DescribeProcess_ValidSummariesGiven_ShouldReturnTwoProcesses(string httpClientResponseXml)
+        {
+            var summaries = new[]
+            {
+                new ProcessSummary {Identifier = "i.gensigset"},
+                new ProcessSummary {Identifier = "i.gensig"}
+            };
+
+            var wpsClient = new WpsClient(new HttpClient(GetMockedMessageHandlerForResponse(httpClientResponseXml)), new XmlSerializationService());
+
+            var collection = await wpsClient.DescribeProcess(MockUri, summaries);
+            collection.Should().HaveCount(2);
+        }
+
+        [Fact]
+        public async Task DescribeProcess_NullUriGiven_ShouldThrowArgumentNullException()
+        {
+            var wpsClient = new WpsClient(new HttpClient(), new XmlSerializationService());
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => wpsClient.DescribeProcess(null, string.Empty));
+        }
+
+        [Fact]
+        public async Task DescribeProcess_EmptyIdentifiersGiven_ShouldThrowArgumentNullException()
+        {
+            var wpsClient = new WpsClient(new HttpClient(), new XmlSerializationService());
+
+            await Assert.ThrowsAsync<InvalidOperationException>(() => wpsClient.DescribeProcess(MockUri, new string[0]));
+        }
+
+    }
+}
+
---
+++ src/Wps/Wps.Client/Services/WpsClient.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Wps.Client.Models;
+using Wps.Client.Models.Execution;
+using Wps.Client.Models.Ows;
+using Wps.Client.Models.Requests;
+using Wps.Client.Models.Responses;
+
+namespace Wps.Client.Services
+{
+    public class WpsClient : IWpsClient, IDisposable
+    {
+
+        private readonly HttpClient _httpClient;
+        private readonly IXmlSerializer _serializationService;
+
+        public WpsClient(HttpClient httpClient, IXmlSerializer serializationService)
+        {
+            _httpClient = httpClient;
+            _serializationService = serializationService;
+        }
+
+        private async Task<string> GetRequestResult(string wpsUri, RequestBase request)
+        {
+            if (wpsUri == null) throw new ArgumentNullException(nameof(wpsUri));
+            if (request == null) throw new ArgumentNullException(nameof(request));
+
+            var requestXml = _serializationService.Serialize(request);
+            var response = await _httpClient.PostAsync(wpsUri,
+                new StringContent(requestXml, Encoding.UTF8));
+
+            var content = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                return content;
+            }
+
+            var exceptionReport = _serializationService.Deserialize<ExceptionReport>(content);
+            var exception = exceptionReport.Exceptions.FirstOrDefault();
+            if (exception != null)
+            {
+                throw exception;
+            }
+
+            throw new Exception("Could not get a valid response from the WPS server.");
+        }
+
+        public async Task<GetCapabilitiesResponse> GetCapabilities(string wpsUri)
+        {
+            if (wpsUri == null) throw new ArgumentNullException(nameof(wpsUri));
+
+            var request = new GetCapabilitiesRequest();
+
+            var content = await GetRequestResult(wpsUri, request);
+            var result = _serializationService.Deserialize<GetCapabilitiesResponse>(content);
+
+            return result;
+        }
+
+        public Task<ProcessOfferingCollection> DescribeProcess(string wpsUri, params ProcessSummary[] processSummaries)
+        {
+            return DescribeProcess(wpsUri, processSummaries.Select(ps => ps.Identifier).ToArray());
+        }
+
+        public async Task<ProcessOfferingCollection> DescribeProcess(string wpsUri, params string[] processIdentifiers)
+        {
+            if (wpsUri == null) throw new ArgumentNullException(nameof(wpsUri));
+            if (processIdentifiers == null) throw new ArgumentNullException(nameof(processIdentifiers));
+
+            if (!processIdentifiers.Any()) throw new InvalidOperationException("You cannot get the description of an empty list of identifiers.");
+
+            var request = new DescribeProcessRequest
+            {
+                Identifiers = processIdentifiers
+            };
+
+            var content = await GetRequestResult(wpsUri, request);
+            var result = _serializationService.Deserialize<ProcessOfferingCollection>(content);
+
+            return result;
+        }
+
+        public Task<StatusInfo> GetJobStatus(string wpsUri, string jobId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResult> GetRawResultAs<TResult>(string wpsUri, ExecuteRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetRawResult(string wpsUri, ExecuteRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<TData>> GetDocumentedResult<TData>(string wpsUri, ExecuteRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _httpClient?.Dispose();
+        }
+    }
+}
